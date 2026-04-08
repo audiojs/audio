6f40c10f4bba4ec6757434e996db4c090c73f91d
Dmitry Iv. <df.creative@gmail.com>
2026-04-08 17:23:01 -0400

    Fix comments

--- fn/split.js
+++ fn/split.js
@@ -1,9 +1,9 @@
-import audio from '../core.js'
+import audio, { parseTime } from '../core.js'
 
 /** Split at offsets, returning views. No copies. */
 audio.fn.split = function(...args) {
-  let offsets = Array.isArray(args[0]) ? args[0] : args
+  let offsets = (Array.isArray(args[0]) ? args[0] : args).map(parseTime)
   let dur = this.duration
-  let cuts = [0, ...[...offsets].sort((a, b) => a - b).filter(t => t > 0 && t < dur), dur]
+  let cuts = [0, ...offsets.sort((a, b) => a - b).filter(t => t > 0 && t < dur), dur]
   return cuts.slice(0, -1).map((start, i) => this.view({at: start, duration: cuts[i + 1] - start}))
 }
